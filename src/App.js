2f7def431a8483469de452fcac03b93c072753b9
Kaylaesmith1 <kaylaesmith1@gmail.com>
2021-02-04 11:34:53 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Weather from './Weather';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Testing
+          <Weather city="Clive"/>
+          </h1>
+          </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export default function Weather(props) {
+  function handleResponse (response) {
+    alert(`The weather in ${response.data.name} is ${Math.round(response.data.main.temp)}ºF`)
+  }
+  let apiKey = "c5ec52f93b5c1b4b6fe696ad9119316f";
+  let units = "imperial";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?q=${props.city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return
+}
